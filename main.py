d2be9a9d6ff35105e46a306878975391129601bc
Saikat <saikatdas45857@gmail.com>
2024-03-20 22:37:39 +0600

    done

--- main.py
+++ main.py
@@ -11,12 +11,3 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
         print(new_letter)
         with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode='w') as letter:
             letter.write(new_letter)
-
-
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
